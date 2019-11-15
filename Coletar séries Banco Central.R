ee6d5580a007b8ef6160dfaff7fd459c7e88c7e9
Felipe Simplício Ferreira <felipe.ferreira@ipea.gov.br>
2019-11-15 14:54:51 -0300

    Mudança de nome e correção de bugs
    Mudança de nome da função para evitar conflito com outras funções que fiz (coletar FRED) e correção de bugs no print do progresso da repetição, que não estava pegando o tamanho do argumento certo e na transformação das colunas do dataframe em numerico (exceto a data) que não estava funcionando quando dado só uma série como argumento

--- Coletar séries Banco Central.R
+++ Coletar séries Banco Central.R
@@ -8,11 +8,12 @@ getwd()
 setwd("C:/Users/User/Documents")
 
 #Criando função para coleta de séries
-coleta_dados = function(series,datainicial="01/03/2011", datafinal = format(Sys.time(), "%d/%m/%Y")){
+coleta_dados_sgs = function(series,datainicial="01/03/2011", datafinal = format(Sys.time(), "%d/%m/%Y")){
   #Argumentos: vetor de séries, datainicial que pode ser manualmente alterada e datafinal que automaticamente usa a data de hoje
   #Cria estrutura de repetição para percorrer vetor com códigos de séries e depois juntar todas em um único dataframe
   for (i in 1:length(series)){
     dados = read.csv(url(paste("http://api.bcb.gov.br/dados/serie/bcdata.sgs.",series[i],"/dados?formato=csv&dataInicial=",datainicial,"&dataFinal=",datafinal,sep="")),sep=";")
+    dados[,-1] = as.numeric(gsub(",",".",dados[,-1])) #As colunas do dataframe em objetos numéricos exceto a da data
     nome_coluna = series[i] #Nomeia cada coluna do dataframe com o código da série
     colnames(dados) = c('data', nome_coluna)
     nome_arquivo = paste("dados", i, sep = "") #Nomeia os vários arquivos intermediários que são criados com cada série
@@ -22,17 +23,16 @@ coleta_dados = function(series,datainicial="01/03/2011", datafinal = format(Sys.
       base = dados1 #Primeira repetição cria o dataframe
     else
       base = merge(base, dados, by = "data", all = T) #Demais repetições agregam colunas ao dataframe criado
-    print(paste(i, length(serie), sep = '/')) #Printa o progresso da repetição
+    print(paste(i, length(series), sep = '/')) #Printa o progresso da repetição
   }
   
   base$data = as.Date(base$data, "%d/%m/%Y") #Transforma coluna de data no formato de data
   base = base[order(base$data),] #Ordena o dataframe de acordo com a data
-  base[,-1]=apply(base[,-1],2,function(x)as.numeric(gsub(",",".",x))) #Transforma o resto do dataframe em objetos numéricos
   return(base)
 }
 
 #Definida a função, podemos criar objetos para guardar os resultados
 serie = c(1,7) #Vetor com código para o dólar e o Ibovespa, respectivamente
-ex = coleta_dados(serie) #Criando objeto em que ficam guardados as séries
+ex = coleta_dados_sgs(serie) #Criando objeto em que ficam guardados as séries
 
 write.csv2(ex, "exemplo.csv", row.names = F) #Salvando arquivo csv em padrão brasileiro
